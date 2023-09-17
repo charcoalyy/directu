ef36627da2ddf99eaff6db4243517a043a796595
Ken Wu <wooqianghao@gmail.com>
2023-09-16 22:21:07 -0400

    changes

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from scrape import scrape_reviews, scrape_descriptions, get_course_name
+from model_cohere import get_similarity_score_liked
 from dotenv import load_dotenv
 load_dotenv()
 import os
@@ -59,3 +60,8 @@ def get_similarity_sources(course_code, course_number):
         return sources
     else:
         return None
+    
+def update_similarity_score(course_code, course_number):
+    score = get_similarity_score_liked(get_similarity_sources(course_code, course_number), course_code, course_number)
+    result = course_collection.update_one({"code" : course_code + course_number}, {"$set" : {"score" : score}})
+    return result
--- backend/test.py
+++ backend/test.py
@@ -3,6 +3,9 @@ from database import get_similarity_sources
 
 # print(get_similarity_sources("cs", "135"))
 
-similarity_score = get_similarity_score_liked([{"course_subject": "cs", "course_num": "136"}], "cs", "135")
+similarity_score = get_similarity_score_liked([{"course_subject": "cs", "course_num": "135"},
+                                               {"course_subject": "math", "course_num": "136"},
+                                               {"course_subject": "stat", "course_num": "240"},
+                                               {"course_subject": "cs", "course_num": "145"}], "cs", "135")
 
 print(similarity_score)
