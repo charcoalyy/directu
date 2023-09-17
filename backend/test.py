97d36e361b3bd5dedadc2a6e8fb820837afd8f94
Ken Wu <wooqianghao@gmail.com>
2023-09-16 23:17:21 -0400

    similarity score mongodb update

--- backend/test.py
+++ backend/test.py
@@ -1,12 +1,2 @@
-from model_cohere import get_similarity_score_liked
-from database import get_similarity_sources
-from connector import feed_liked_courses_here
-import time
-
-# print(get_similarity_sources("cs", "135"))
-
-START_TIME = time.time()
-similarity_score = feed_liked_courses_here(["cs135", "cs136", "math235"], "cs", "135")
-END_TIME = time.time()
-
-print(f"Elapsed time: {END_TIME - START_TIME:.2f} seconds", )
+from database import update_similarity_score
+update_similarity_score(['cs135', 'cs136'], "cs", "246")
