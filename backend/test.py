f151a2ff8448d445beab9c8d8a88d76641bdd6fb
Ken Wu <wooqianghao@gmail.com>
2023-09-16 21:06:55 -0400

    changes

--- backend/test.py
+++ backend/test.py
@@ -1,11 +1,3 @@
-from database import update_term
-from scrape import get_course_list
-from random import randint
+from database import get_similarity_sources
 
-terms = ['1A, 1B, 2A, 2B, 3A, 3B, 4A, 4B']
-
-courses = get_course_list()
-
-for course in courses:
-    for course_num in courses[course]:
-        update_term(course + course_num, terms[randint(0, len(terms) - 1)])
+print(get_similarity_sources("cs", "135"))
